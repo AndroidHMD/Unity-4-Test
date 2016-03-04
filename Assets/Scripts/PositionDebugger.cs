485747d3a65c67268e6adb8c5ac3e56a5c216be7
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-03-04 15:59:44 +0100

    Disable video render, add pos debug + import skybox asset

---
+++ Assets/Scripts/PositionDebugger.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionDebugger : MonoBehaviour {
+	void Update () {
+		Debug.Log(gameObject.name + " position: " + gameObject.transform.position);
+	}
+}
