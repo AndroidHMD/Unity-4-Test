0d5e6bb57b8fbeafe79a585945d151e2f7b9b5fd
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-03-07 13:24:19 +0100

    Change PositionDebugger to TransformDebugger that enables additional Transform debugging

--- Assets/Scripts/PositionDebugger.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class PositionDebugger : MonoBehaviour {
-	void Update () {
-		Debug.Log(gameObject.name + " position: " + gameObject.transform.position);
-	}
-}
