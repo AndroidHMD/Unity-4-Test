b410d6e751b121cd92f0be58b7a14785f8cc3c4a
Benjamin Wiberg <benjaminwiberg@gmail.com>
2016-03-07 14:00:45 +0100

    Disable rendering of cube target sides

---
+++ Assets/Scripts/RemoveVuforiaComponentsFromChildren.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoveVuforiaComponentsFromChildren : MonoBehaviour {
+	public GameObject targetsParent;
+
+	void Start () {
+		int count = 0;
+
+		foreach (Transform childTransform in targetsParent.transform)
+		{
+			if (childTransform != transform)
+			{
+				// childTransform is a child
+				var child = childTransform.gameObject;
+
+				var childMaskOutBehaviour = child.GetComponent<Vuforia.MaskOutBehaviour>();
+				var childMeshRenderer = child.GetComponent<MeshRenderer>();
+				var childMeshFilter = child.GetComponent<MeshFilter>();
+
+				Destroy(childMaskOutBehaviour);
+				Destroy(childMeshRenderer);
+				Destroy(childMeshFilter);
+
+				++count;
+			}
+		}
+
+		Debug.Log ("Removed " + count + " MaskOutBehaviours from children");
+	}
+}
